65206bd3d0c5813da75f156eb6d571be13a800a4
jyj <jyj202040@gmail.com>
2022-03-29 13:33:23 +0900

    commit

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,6 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
-/*
-import React,{useState} from 'react';
-import ExpenseDate from './ExpenseDate';*/
+
 function ExpenseItem(props) {
   /*
     const [title,setTitle]=useState(props.title);
@@ -12,27 +11,13 @@ function ExpenseItem(props) {
 
   return (
     <div className="expense-item">
-      <div>
-        <div>{props.date.toLocaleString("en-US", { month: "long" })}</div>
-        <div>{props.date.getFullYear()}</div>
-        <div>{props.date.toLocaleString("en-US", { day: "2-digit" })}</div>
-      </div>
+      <ExpenseDate date={props.date} />
 
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
-    /*
-        <div className="expense-item">
-            <ExpenseDate date={props.date}/>
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">${props.amount}</div>
-            </div>
-            <button onClick={click}>change title</button>
-        </div>
-        */
   );
 }
 
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,19 +1,32 @@
-import ExpenseItem from './ExpenseItem.js';
+import ExpenseItem from "./ExpenseItem.js";
+import "./Expenses.css";
+function Expenses(props) {
+  const expenses = props.expenses;
 
-function Expenses(props){
-    
-
-    return(
-        <div>
-            <ExpenseItem  title={props.expenses[0].title} amount={props.expenses[0].amount} date={props.expenses[0].date}/>
-            <ExpenseItem  title={props.expenses[1].title} amount={props.expenses[1].amount} date={props.expenses[1].date}/>
-            <ExpenseItem  title={props.expenses[2].title} amount={props.expenses[2].amount} date={props.expenses[2].date}/>
-            <ExpenseItem  title={props.expenses[3].title} amount={props.expenses[3].amount} date={props.expenses[3].date}/>
-        </div>
-    );
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
+    </div>
+  );
 }
-
-
-
 
 export default Expenses;
