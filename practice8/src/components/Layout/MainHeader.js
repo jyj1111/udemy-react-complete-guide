badd770fb7bb946675083ab8768d060f718bc3cc
jyj <jyj202040@gmail.com>
2022-05-07 21:51:28 +0900

    ...

--- practice8/src/components/Layout/MainHeader.js
+++ practice8/src/components/Layout/MainHeader.js
@@ -1,12 +1,25 @@
-import CartButton from '../Cart/CartButton';
-import classes from './MainHeader.module.css';
-
+import CartButton from "../Cart/CartButton";
+import classes from "./MainHeader.module.css";
+import { useDispatch } from "react-redux";
+import { authActions } from "../../store/auth-slice";
 const MainHeader = (props) => {
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
   return (
     <header className={classes.header}>
       <h1>ReduxCart</h1>
       <nav>
         <ul>
+          <li>
+            <a href="/">My Info</a>
+          </li>
+          <li>
+            <a href="/" onClick={logoutHandler}>
+              Logout
+            </a>
+          </li>
           <li>
             <CartButton />
           </li>
